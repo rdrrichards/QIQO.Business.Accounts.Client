047c9aec5455d81360eee7fecf03e311778338db
R Richards <rdrrichards@hotmail.com>
2017-08-20 14:43:08 -0500

    a little refactoring

--- src/app/account/account-list.component.ts
+++ src/app/account/account-list.component.ts
@@ -11,6 +11,7 @@ import 'rxjs/add/operator/map';
 import { IAccount, IAccountViewModel } from '../models/account';
 import { AccountService } from '../services/account.service';
 import { Subscription } from 'rxjs/Subscription';
+import { AccountDataSource } from './account.datasource';
 
 @Component({
     selector: 'app-account-list',
@@ -35,35 +36,9 @@ export class AccountListComponent implements OnInit, OnDestroy {
             this.dataSource = new AccountDataSource(this.accountService, companyId, this.paginator);
         }
         );
-        // console.log(JSON.stringify(this.dataSource.connect()));
     }
 
     ngOnDestroy(): void {
         this.paramSubscription.unsubscribe();
     }
-}
-
-export class AccountDataSource extends DataSource<any> {
-    constructor(private accountService: AccountService,
-        private companyId: number,
-        private _paginator: MdPaginator) {
-        super();
-    }
-
-    /** Connect function called by the table to retrieve one stream containing the data to render. */
-    connect(): Observable<IAccountViewModel[]> {
-        const displayDataChanges = [
-            this.accountService.dataChange,
-            this._paginator.page,
-        ];
-        return Observable.merge(...displayDataChanges).map(() => {
-            const data = this.accountService.data.slice();
-
-            // Grab the page's slice of data.
-            const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
-            return data.splice(startIndex, this._paginator.pageSize);
-        });
-    }
-
-    disconnect() { }
 }
---
+++ src/app/account/account.datasource.ts
@@ -0,0 +1,33 @@
+import { DataSource } from '@angular/cdk';
+import { MdPaginator } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+
+import { IAccount, IAccountViewModel } from '../models/account';
+import { AccountService } from '../services/account.service';
+
+export class AccountDataSource extends DataSource<any> {
+    constructor(private accountService: AccountService,
+        private companyId: number,
+        private _paginator: MdPaginator) {
+        super();
+    }
+
+    /** Connect function called by the table to retrieve one stream containing the data to render. */
+    connect(): Observable<IAccountViewModel[]> {
+        const displayDataChanges = [
+            this.accountService.dataChange,
+            this._paginator.page,
+        ];
+        return Observable.merge(...displayDataChanges).map(() => {
+            const data = this.accountService.data.slice();
+
+            // Grab the page's slice of data.
+            const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+            return data.splice(startIndex, this._paginator.pageSize);
+        });
+    }
+
+    disconnect() { }
+}
