113368b5d828242f5395b17c54c6791c2c8945cd
R. Richards <rdrrichards@hotmail.com>
2017-10-12 18:19:25 -0500

    new md changes are fixed

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -4,11 +4,11 @@ import { CONFIG } from '../common/config';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
+// import 'rxjs/add/operator/do';
+// import 'rxjs/add/operator/catch';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-import { IAccount, IAccountViewModel } from '../models/account';
+import { IAccountViewModel } from '../models/account';
 
 @Injectable()
 export class AccountService {
