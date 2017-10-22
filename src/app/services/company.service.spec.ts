5ce4aa0a29210b64112ce637f5795496d14ea904
R Richards <rdrrichards@hotmail.com>
2017-10-22 13:09:38 -0500

    company spec in new format

--- src/app/services/company.service.spec.ts
+++ src/app/services/company.service.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientModule, HttpRequest, HttpParams } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CompanyService } from './company.service';
 
 describe('CompanyService', () => {
-  let service: CompanyService;
 
-  beforeEach(
-    async(() => {
-      service = new CompanyService(null);
-    })
-  );
-
-  it('should create', () => {
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule, HttpClientTestingModule ],
+      providers: [ CompanyService ]
+    });
   });
+
+  afterEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+    backend.verify();
+  }));
+
+  it(`should create`, async(inject([CompanyService, HttpTestingController],
+    (service: CompanyService, backend: HttpTestingController) => {
+      expect(service).toBeTruthy();
+  })));
+
 });
