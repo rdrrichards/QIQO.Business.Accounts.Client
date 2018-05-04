10a601ea2cdeb2c9dd224b0ac66523705518246b
R. Richards <rdrrichards@hotmail.com>
2018-05-04 05:30:37 -0500

    ng update on application

--- karma.conf.js
+++ karma.conf.js
@@ -8,19 +8,19 @@ module.exports = function (config) {
       {pattern: './src/app/testing', included: false, watched: true},
       {pattern: './node_modules/@angular/material/prebuilt-themes/indigo-pink.css', included: true, watched: true},
     ],
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly', 'text-summary'],
       dir: path.join(__dirname, 'coverage'),
       fixWebpackSourcePaths: true,
       skipFilesWithNoCoverage: true,
