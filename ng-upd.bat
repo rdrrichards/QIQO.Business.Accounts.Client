79abe85537d48324fa6f1b57348888e974d4914e
R. Richards <rdrrichards@hotmail.com>
2019-06-14 15:30:17 -0500

    batch

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,3 +1,6 @@
-REM CALL ng update @angular/cli
+CALL ng update @angular/cli
+CALL git commit -am "ng cli updates to latest"
 CALL ng update @angular/core
+CALL git commit -am "ng updates to latest"
 CALL ng update @angular/material
+CALL git commit -am "ng material to latest"
