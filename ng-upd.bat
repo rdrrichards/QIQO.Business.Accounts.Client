d3f93fb268349bd06c78e30baa11442c0fb6f6b4
R. Richards <rdrrichards@hotmail.com>
2019-09-25 14:45:25 -0500

    pre updates commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,7 +1,7 @@
 CALL git commit -am "pre updates commit"
 CALL ng update @angular/cli --force
 CALL git commit -am "ng cli updates to latest"
-CALL ng update @angular/core
+CALL ng update @angular/core --force
 CALL git commit -am "ng updates to latest"
 CALL ng update @angular/material
 CALL git commit -am "ng material to latest"
